1a53fade0116c1a8649210e0edd5f363fb95ba3a
Dillon Kearns <dillon@dillonkearns.com>
2017-02-23 13:01:49 -0800

    Show inventory

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (main)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Cart
 
 
@@ -9,17 +10,55 @@ type Msg
 
 
 type alias Model =
-    Cart.Cart
+    { cart : Cart.Cart
+    , inventory : List Cart.Item
+    }
+
+
+viewCartTotal : Cart.Cart -> Html Msg
+viewCartTotal cart =
+    text <| "Cart total is: " ++ (cart |> Cart.total |> toString)
+
+
+viewInventory : List Cart.Item -> Html Msg
+viewInventory inventory =
+    div []
+        [ h1 [] [ text "Inventory" ]
+        , ul [] (List.map viewInventoryItem inventory)
+        ]
+
+
+viewInventoryItem : Cart.Item -> Html Msg
+viewInventoryItem item =
+    div [] [ text item.name, viewCost item.cost ]
+
+
+quatlooBadge : Html Msg
+quatlooBadge =
+    img [ src "./assets/quatloo.png", style [ ( "max-width", "10px" ), ( "padding-left", "3px" ), ( "padding-right", "3px" ) ] ] []
+
+
+viewCost : Int -> Html Msg
+viewCost int =
+    span [] [ quatlooBadge, text (toString int) ]
 
 
 view : Model -> Html Msg
 view model =
-    text <| "Cart total is: " ++ (model |> Cart.total |> toString)
+    div []
+        [ div [] [ viewInventory model.inventory ]
+        , div [] [ viewCartTotal model.cart ]
+        ]
+
+
+inventory : List Cart.Item
+inventory =
+    [ Cart.Item "Phaser" 456 ]
 
 
 init : ( Model, Cmd Msg )
 init =
-    Cart.empty ! []
+    { cart = Cart.empty, inventory = inventory } ! []
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
