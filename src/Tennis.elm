3a6579f92b11a7e082e2e000c15d0b820f2b4eaa
Dillon Kearns <dillon@dillonkearns.com>
2018-01-30 09:06:33 -0800

    Add possible solution code for Tennis module.

--- src/Tennis.elm
+++ src/Tennis.elm
@@ -1,28 +1,79 @@
-module Tennis exposing (..)
+module Tennis exposing (gameScoreToString, init, player1Scores, player2Scores)
 
 
 type alias Game =
-    Int
+    ( Int, Int )
 
 
 init : Game
 init =
-    0
+    ( 0, 0 )
 
 
 player1Scores : Game -> Game
-player1Scores game =
-    1
+player1Scores ( player1, player2 ) =
+    ( player1 + 1, player2 )
 
 
-scoreToString : Game -> String
-scoreToString game =
-    case game of
+player2Scores : Game -> Game
+player2Scores ( player1, player2 ) =
+    ( player1, player2 + 1 )
+
+
+gameScoreToString : Game -> String
+gameScoreToString (( player1Score, player2Score ) as game) =
+    if isTiedScore game then
+        tiedScoreToString player1Score
+    else if isAdScore game then
+        if player1Score > player2Score then
+            "Ad Player 1"
+        else
+            "Ad Player 2"
+    else if player1Score > 3 then
+        "Player 1 wins"
+    else if player2Score > 3 then
+        "Player 2 wins"
+    else
+        playerScoreToString player1Score ++ "-" ++ playerScoreToString player2Score
+
+
+tiedScoreToString : Int -> String
+tiedScoreToString tiedScore =
+    if tiedScore >= 3 then
+        "Deuce"
+    else
+        playerScoreToString tiedScore ++ "-all"
+
+
+isAdScore : Game -> Bool
+isAdScore ( player1Score, player2Score ) =
+    (max player1Score player2Score > 3)
+        && (player2Score
+                - player1Score
+                |> abs
+                |> (\difference -> difference == 1)
+           )
+
+
+isTiedScore : Game -> Bool
+isTiedScore ( player1Score, player2Score ) =
+    player1Score == player2Score
+
+
+playerScoreToString : Int -> String
+playerScoreToString score =
+    case score of
         0 ->
-            "Love-Love"
+            "Love"
 
         1 ->
-            "15-Love"
+            "15"
+
+        2 ->
+            "30"
+
+        3 ->
+            "40"
 
         _ ->
             "Unhandled score"
--- tests/TennisTest.elm
+++ tests/TennisTest.elm
@@ -11,12 +11,103 @@ suite =
         [ test "new game" <|
             \_ ->
                 Tennis.init
-                    |> Tennis.scoreToString
-                    |> Expect.equal "Love-Love"
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "Love-all"
         , test "player 1 scores" <|
             \_ ->
                 Tennis.init
                     |> Tennis.player1Scores
-                    |> Tennis.scoreToString
+                    |> Tennis.gameScoreToString
                     |> Expect.equal "15-Love"
+        , test "player 1 scores two times" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "30-Love"
+        , test "player 1 scores three times" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "40-Love"
+        , test "player 1 ace game" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "Player 1 wins"
+        , test "player 2 ace game" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "Player 2 wins"
+        , test "player 2 scores" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player2Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "Love-15"
+        , test "players are tied" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "15-all"
+        , test "deuce" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "Deuce"
+        , test "just under ad" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "40-30"
+        , test "ad-in" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "Ad Player 1"
+        , test "ad-out" <|
+            \_ ->
+                Tennis.init
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player1Scores
+                    |> Tennis.player2Scores
+                    |> Tennis.gameScoreToString
+                    |> Expect.equal "Ad Player 2"
         ]
