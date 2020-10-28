0c78cb916288b944f4979d356d6b9bf7331878d7
Dillon Kearns <dillon@dillonkearns.com>
2020-10-28 13:21:50 -0700

    Add latency.

--- tcr.sh
+++ tcr.sh
@@ -1,4 +1,4 @@
-fswatch -0 $(git ls-files) | while read -d "" event
+fswatch -0 $(git ls-files) --latency=1 | while read -d "" event
 do
     npx elm-test && git commit -am 'TCR.' || git reset --hard
 done
